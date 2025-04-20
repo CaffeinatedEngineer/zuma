e5ed95c9f96ee6646d8e412c48e1ec17b6010ae5
ShashwatMishra2411 <mishrashashwat.sharat@gmail.com>
2025-04-20 19:45:26 +0530

    Refactor header component for improved navigation and styling

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -6,20 +6,11 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Header() {
   return (
-    <nav
-      className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto"
-    >
+    <nav className="container flex items-center justify-between py-4 lg:px-8 px-2 mx-auto">
       <div className="flex lg:flex-1">
-        <NavLink
-          href="/"
-          className="flex items-center gap-1 lg:gap-2 shrink-0"
-        >
-          <FileText
-            className="h-5 w-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition"
-          />
-          <span className="font-extrabold lg:text-xl text-gray-900">
-            Zuma
-          </span>
+        <NavLink href="/" className="flex items-center gap-1 lg:gap-2 shrink-0">
+          <FileText className="h-5 w-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12 transform transition" />
+          <span className="font-extrabold lg:text-xl text-gray-900">Zuma</span>
         </NavLink>
       </div>
 
@@ -42,7 +33,7 @@ export default function Header() {
         </SignedIn>
 
         <SignedOut>
-          <NavLink href="/#signin">Sign In</NavLink>
+          <NavLink href="/sign-in">Sign In</NavLink>
         </SignedOut>
       </div>
     </nav>
