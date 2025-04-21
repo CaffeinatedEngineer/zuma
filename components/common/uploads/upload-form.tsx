9ec274e37e7e32bf1d4b0f6a604e597a053ede66
Caffeinated Engineer <shashankshiv.jha@gmail.com>
2025-04-21 16:28:31 +0530

    Refactor UploadForm to integrate file upload handling and improve error notifications

--- components/common/uploads/upload-form.tsx
+++ components/common/uploads/upload-form.tsx
@@ -1,6 +1,7 @@
 'use client'
 import UploadFormInput from "@/components/common/uploads/upload-form-input";
-
+import { useUploadThing } from "@/utils/uploadthing";
+import { toast } from "@/components/ui/sonner"; // Importing the useToast hook from the sonner library
 import { z } from "zod";
 const schema = z.object({
     file: z.instanceof(File, { message: 'Invalid file type' })
@@ -12,7 +13,28 @@ const schema = z.object({
 });
 
 export default function UploadForm() {
-    const handleSubmit=(
+    const {toast} = useToast();
+    const { startUpload, routeConfig } = useUploadThing(
+      'pdfUploader', {
+        onClientUploadComplete: () => {
+          console.log('uploaded successfully!');
+        },
+        onUploadError: (err) => {
+          console.error('error occurred while uploading', err);
+          toast({
+            title: 'Upload failed',
+            description: 'err.message',
+          })
+        },
+        onUploadBegin: ({ file }) => {
+          console.log('upload has begun for', file);
+        },
+      }
+    );
+
+
+
+    const handleSubmit=async(
         e:React.FormEvent<HTMLFormElement>
     )=> {
         e.preventDefault();
@@ -25,20 +47,51 @@ export default function UploadForm() {
         const validatedFields = schema.safeParse({ file });
 
         if(!validatedFields.success) {
-            console.log(validatedFields.error.flatten().fieldErrors.file?.[0]?? 'Invalid file');
+            console.log(validatedFields.error.flatten().
+            fieldErrors.file?.[0]?? 'Invalid file');
+            toast({
+                title: '❌ Something went wrong',
+                variant: 'destructive',
+                description: validatedFields.error.flatten().
+                fieldErrors.file?.[0]?? 'Invalid file'
+                
+                
+            });
             return;
         }
+        toast({
+            title: '📄Processing PDF...',
+            description: 'Hang tight ! Your file is being processed',});
         //schema with zod
         //upload the file to uploadthing
+        
+
+        const resp = await startUpload([file]);
+        if (!resp) {
+            toast({
+                title: '❌ Something went wrong',
+                variant: 'destructive',
+                description: 'Error uploading file. Use a different file',
+            });
+            return;
+        }
+        toast({
+            title: '📄Uploading PDF...',
+            description: 'We are uploading your PDF',});
+        
         //parse the pdf using lang chain
         //summarizze the pdf using AI
         //save the summary to database
         //redirect to the [id] summary page
-    }
+    };
     return (
         <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
             <UploadFormInput onSubmit={handleSubmit} />
         </div>
     );
+}
+
+function useToast(): { toast: any; } {
+    throw new Error("Function not implemented.");
 }
 
